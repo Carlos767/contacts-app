c1bf895164d995d1abdf7c3a7a4da6fd36409275
Carlos <school@Carlos-MacBook-Air.local>
2017-01-24 19:29:11 -0800

    sooo many updates!

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,56 @@
 class ContactsController < ApplicationController
 
-  def view_contacts
-    @contacts = Contacts.all
+  def contacts
+    @contacts = Contact.all
     render "contacts.html.erb"
   end
+
+  def new
+    render "new.html.erb"
+  end
+
+  def create
+    contact = Contact.create(
+      first_name: params[:first_name], 
+      last_name: params[:last_name],
+      email: params[:email]
+      phone: params[:phone]
+      image: params[:image])
+    flash[:success] = "Contact successfully created!"
+    redirect_to "/contacts/#{contact_id}"
+  end
+
+  def show
+  contact_id = params[:id]
+    @contact = Contact.find_by(id: contact_id)
+    render "show.html.erb"
+  end
+
+  def edit
+    contact_id = params[:id]
+    @contact = Contact.find_by(id: contact_id)
+    render "edit.html.erb"
+  end
+
+  def update
+    contact_id = params[:id]
+    contact = Contact.find_by(id: contact_id)
+    contact.update(
+      first_name: params[:first_name], 
+      last_name: params[:last_name],
+      email: params[:email]
+      phone: params[:phone]
+      image: params[:image])
+    flash[:success] = "Contact successfully updated!"
+    redirect_to "/contacts/#{contact_id}"
+  end
+
+  def destroy
+    contact_id = params[:id]
+    contact = Contact.find_by(id: contact_id)
+    contact.destroy
+    flash[:success] = "Contact successfully deleted!"
+    redirect_to "/contacts/#{contact_id}"
+  end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
 
-  get "/contacts" => contacts#"view_contacts"
+  get "/contacts" => "contacts#index"
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/contacts/new" => "contacts#new"
+
+  post "/contacts" => "contacts#create"
+
+  get "/contacts/:id" => "contacts#show"
+
+  get "/contacts/:id/edit" => "contacts#edit"
+
+  patch "contacts/:id" => "contacts#update"
+
+  delete "contact/:id" => "contacts#destroy"
 end
+
+  
+
+
